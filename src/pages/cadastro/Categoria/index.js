4b425ed253dcc147bfda25e73915fc94875b4bbd
Tiago Jaques <skasax@gmail.com>
2020-07-31 00:15:04 -0300

    Atualizacao das URLS do FETCH

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -31,7 +31,7 @@ function CadastroCategoria() {
 
   useEffect(() => {
     if (window.location.href.includes('localhost')) {
-      const URL = 'http://localhost:8080/categorias';
+      const URL = window.location.hostname.includes('localhost') ? 'http://localhost:8080/categorias' : 'https://tiagojaques.herokuapp.com/categorias';
       fetch(URL)
         .then(async (respostaDoServer) => {
           if (respostaDoServer.ok) {
